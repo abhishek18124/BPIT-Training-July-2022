ab86b9f28bb0545596a3f925e791c14a34c864a7
abhishek18124 <abhishek18124@iiitd.ac.in>
2022-09-20 21:27:57 +0530

    Lecture 37

---
+++ 03 - September/Lecture 37 - 20thSeptember2022/004FindMaximum.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+
+using namespace std;
+
+int main() {
+
+	// int arr[] = {1, 4, 0, 1, 2, 3, 5};
+	int arr[] = {4, 3, 1, 2, 0, 5};
+	int n = sizeof(arr) / sizeof(int);
+	//int k = 4;
+	int k = 3;
+
+	int i = 0;     // to store the start of the window
+	int j = 0;     // to store the end of the window
+	deque<int> q;  // to store numbers of interest in a window
+	vector<int> v; // to store the maximum element for each k-sized window
+	
+	// find the maximum element in the 1st window
+	
+	while(j < k) {
+		while(!q.empty() && q.back() < arr[j]) q.pop_back();
+		q.push_back(arr[j]);
+		j++;
+	}
+
+	v.push_back(q.front());
+	
+	// find the maximum element in the remaining windows
+		
+	while(j < n) {
+
+		// slide the window
+		if(arr[i] == q.front()) q.pop_front();
+		i++;
+		while(!q.empty() && q.back() < arr[j]) q.pop_back();
+		q.push_back(arr[j]);
+
+		v.push_back(q.front());
+
+		j++;
+
+	}
+
+	for(int i=0; i<v.size(); i++) {
+		cout << v[i] << " ";
+	}
+
+	cout << endl;
+	
+	return 0;
+}
+
